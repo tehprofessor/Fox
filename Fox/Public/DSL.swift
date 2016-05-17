3351350e235298dd8d05e08c6ca654c90ca91212
Seve <servando@gmail.com>
2016-05-17 12:37:18 -0700

    Support for Swift 2 and iOS 9.

--- Fox/Public/DSL.swift
+++ Fox/Public/DSL.swift
@@ -2,25 +2,25 @@ import Foundation
 import XCTest
 
 public func Assert(
-    property: FOXGenerator,
-    seed: UInt? = nil,
-    numberOfTests: UInt? = nil,
-    maximumSize: UInt? = nil,
-    file: String = __FILE__,
-    line: UInt = __LINE__) {
-
-        let theSeed = (seed != nil) ? seed! : FOXGetSeed()
-        let numTests = (numberOfTests != nil) ? numberOfTests! : FOXGetNumberOfTests()
-        let maxSize = (maximumSize != nil) ? maximumSize! : FOXGetMaximumSize()
-
-        let runner = FOXRunner.assertInstance()
-        let result = runner.resultForNumberOfTests(numTests,
-            property: property,
-            seed: theSeed,
-            maxSize: maxSize)
-
-        if !result.succeeded {
-            XCTFail("Property failed with: \(result.singleLineDescriptionOfSmallestValue())",
-                file: file, line: line)
-        }
+  property: FOXGenerator,
+  seed: UInt? = nil,
+  numberOfTests: UInt? = nil,
+  maximumSize: UInt? = nil,
+  file: StaticString = #file,
+  line: UInt = #line) {
+  
+  let theSeed = (seed != nil) ? seed! : FOXGetSeed()
+  let numTests = (numberOfTests != nil) ? numberOfTests! : FOXGetNumberOfTests()
+  let maxSize = (maximumSize != nil) ? maximumSize! : FOXGetMaximumSize()
+  
+  let runner = FOXRunner.assertInstance()
+  let result = runner.resultForNumberOfTests(numTests,
+                                             property: property,
+                                             seed: theSeed,
+                                             maxSize: maxSize)
+  
+  if !result.succeeded {
+    XCTFail("Property failed with: \(result.singleLineDescriptionOfSmallestValue())",
+            file: file, line: line)
+  }
 }
